0731ae389d006f374450cb8f5b825e4dfbac4e2c
ARS-TECNOLOGIC <anselmoroberto003@gmail.com>
2025-05-10 11:19:29 -0300

    Cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 
 app.use(express.json());
+app.use(cors());
 app.get('/efi/:loc', (req, res) => {
     res.send(`Location: ${req.params.loc}`);
 });
