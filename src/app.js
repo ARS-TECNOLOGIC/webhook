eb232bd07ad2fc5e98e33769ac169c050bd615b0
ARS-TECNOLOGIC <anselmoroberto003@gmail.com>
2025-05-13 18:35:00 -0300

    Atualizar

--- src/app.js
+++ src/app.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const cors = require('cors');
-const routes = require('./routes/index.js'); // Import the routes
+// const routes = require('./routes/index.js'); // Import the routes
 
 const app = express();
-
+const route = express.Router();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api', routes); // Use the imported routes
-
-// app.use('/api', routes); // Use the imported routes
-
+route.get('/', async (req, resp) => {
+    resp.status(200).json({ OK: 'API is running!' });
+}); // Route to check if the API is running
 
 module.exports = app;
 // app.js
