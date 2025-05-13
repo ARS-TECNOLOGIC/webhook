8c9fe84239f3dcaa88ace1bae65c4a8fedc4b4ed
ARS-TECNOLOGIC <anselmoroberto003@gmail.com>
2025-05-13 14:22:20 -0300

    Atualizar

--- src/routes/efi.route.js
+++ src/routes/efi.route.js
@@ -1,7 +1,9 @@
 const route = require('express').Router();
 const efiController = require('../controllers/efi.controller.js');
 
-route.get('/estanciar', efiController.estanciarEfi); // Route to estanciar Efi
+route.get('/estanciar', async(req,resp)=>{
+    resp.status(200).json({OK:'Certificado carregado!'});
+}); // Route to estanciar Efi
 
 
 module.exports = route; // Export the router for use in the main application file
