b9344ea1b586b62edce36c64b7c007f43e34e470
ARS-TECNOLOGIC <anselmoroberto003@gmail.com>
2025-05-13 18:19:59 -0300

    Atualizar o Vercel.json

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
 const routes = require('./routes/index.js'); // Import the routes
+
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 app.use('/api', routes); // Use the imported routes
+
+// app.use('/api', routes); // Use the imported routes
+
 
 module.exports = app;
 // app.js
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,9 @@ const route = require('express').Router();
 const efiRoute = require('./efi.route.js');
 
 
-route.use('/efi', efiRoute); // Use the efi routes
+// route.use('/efi', efiRoute); // Use the efi routes
+route.get('/', async (req, resp) => {
+    resp.status(200).json({ OK: 'API is running!' });
+}); // Route to check if the API is running
 
 module.exports = route; // Export the router for use in the main application file
