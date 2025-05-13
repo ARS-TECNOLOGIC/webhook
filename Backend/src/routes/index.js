49e9bbd61719fdc0c4698a94922e895acd28df1b
ARS-TECNOLOGIC <anselmoroberto003@gmail.com>
2025-05-13 19:23:49 -0300

    Atualizar

--- Backend/src/routes/index.js
+++ Backend/src/routes/index.js
@@ -1,10 +1,8 @@
 const route = require('express').Router();
-// const efiRoute = require('./efi.route.js');
+const efiRoute = require('./efi.route.js');
 
 
-// route.use('/efi', efiRoute); // Use the efi routes
-route.get('/', async (req, resp) => {
-    resp.status(200).json({ OK: 'API is running!' });
-}); // Route to check if the API is running
+route.use('/efi', efiRoute); // Use the efi routes
+
 
 module.exports = route; // Export the router for use in the main application file
