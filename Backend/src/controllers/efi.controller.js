16b11c3cb62dbbbc4d1d3943acdd972dd6c9db46
ARS-TECNOLOGIC <anselmoroberto003@gmail.com>
2025-05-13 19:31:23 -0300

    Atualizar

--- Backend/src/controllers/efi.controller.js
+++ Backend/src/controllers/efi.controller.js
@@ -3,27 +3,29 @@ const path = require('path');
 const EfiPay = require('sdk-node-apis-efi');
 
 const estanciarEfi = (req, res) => {
-  try {
-    // Recuperar e salvar o certificado no /tmp (único diretório gravável na Vercel)
-    const certBase64 = process.env.EFI_CERT_BASE64;
-    const certBuffer = Buffer.from(certBase64, 'base64');
-    const certPath = path.join('/tmp', 'certificado.p12');
-    fs.writeFileSync(certPath, certBuffer);
+  const certBase64 = process.env.EFI_CERT_BASE64;
+  console.log('Certificado Base64:', certBase64);
+  // try {
+  //   // Recuperar e salvar o certificado no /tmp (único diretório gravável na Vercel)
+  //   const certBase64 = process.env.EFI_CERT_BASE64;
+  //   const certBuffer = Buffer.from(certBase64, 'base64');
+  //   const certPath = path.join('/tmp', 'certificado.p12');
+  //   fs.writeFileSync(certPath, certBuffer);
 
-    // Inicializar a SDK
-    const efi = new EfiPay({
-      client_id: process.env.EFI_CLIENT_ID,
-      client_secret: process.env.EFI_CLIENT_SECRET,
-      certificate: certPath,
-      sandbox: true, // true se for ambiente de teste
-    });
+  //   // Inicializar a SDK
+  //   const efi = new EfiPay({
+  //     client_id: process.env.EFI_CLIENT_ID,
+  //     client_secret: process.env.EFI_CLIENT_SECRET,
+  //     certificate: certPath,
+  //     sandbox: true, // true se for ambiente de teste
+  //   });
   
-    res.status(200).json({ OK: 'Certificado carregado!' });
+  //   res.status(200).json({ OK: 'Certificado carregado!' });
 
-  } catch (error) {
-    console.error(error.response?.data || error.message);
-    res.status(500).json({ error});
-  }
+  // } catch (error) {
+  //   console.error(error.response?.data || error.message);
+  //   res.status(500).json({ error});
+  // }
 };
 
 module.exports = {
