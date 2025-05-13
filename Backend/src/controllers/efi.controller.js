f211524c30045969a3e9a965a9c3a1b80af5951f
ARS-TECNOLOGIC <anselmoroberto003@gmail.com>
2025-05-13 19:46:36 -0300

    Atualizar

--- Backend/src/controllers/efi.controller.js
+++ Backend/src/controllers/efi.controller.js
@@ -4,30 +4,28 @@
 require('dotenv').config();
 
 const estanciarEfi = (req, res) => {
-  const certBase64 = process.env.EFI_CERT_BASE64;
-  console.log('Certificado Base64:', certBase64);
-  res.status(200).json({ OK: 'Certificado carregado!' });
-  // try {
-  //   // Recuperar e salvar o certificado no /tmp (único diretório gravável na Vercel)
-  //   const certBase64 = process.env.EFI_CERT_BASE64;
-  //   const certBuffer = Buffer.from(certBase64, 'base64');
-  //   const certPath = path.join('/tmp', 'certificado.p12');
-  //   fs.writeFileSync(certPath, certBuffer);
-
-  //   // Inicializar a SDK
-  //   const efi = new EfiPay({
-  //     client_id: process.env.EFI_CLIENT_ID,
-  //     client_secret: process.env.EFI_CLIENT_SECRET,
-  //     certificate: certPath,
-  //     sandbox: true, // true se for ambiente de teste
-  //   });
   
-  //   res.status(200).json({ OK: 'Certificado carregado!' });
+  try {
+    // Recuperar e salvar o certificado no /tmp (único diretório gravável na Vercel)
+    const certBase64 = process.env.EFI_CERT_BASE64;
+    const certBuffer = Buffer.from(certBase64, 'base64');
+    const certPath = path.join('/tmp', 'certificado.p12');
+    fs.writeFileSync(certPath, certBuffer);
 
-  // } catch (error) {
-  //   console.error(error.response?.data || error.message);
-  //   res.status(500).json({ error});
-  // }
+    // Inicializar a SDK
+    const efi = new EfiPay({
+      client_id: process.env.EFI_CLIENT_ID,
+      client_secret: process.env.EFI_CLIENT_SECRET,
+      certificate: certPath,
+      sandbox: true, // true se for ambiente de teste
+    });
+  
+    res.status(200).json({ OK: 'Certificado carregado!' });
+
+  } catch (error) {
+    console.error(error.response?.data || error.message);
+    res.status(500).json({ error});
+  }
 };
 
 module.exports = {
